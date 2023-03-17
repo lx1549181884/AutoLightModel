2f36ca03b9180861030c2862accece61ebb0050a
Rick <1549181884@qq.com>
2023-03-17 20:46:41 +0800

    init

---
+++ app/src/main/java/com/rick/autolightmodel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rick.autolightmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
